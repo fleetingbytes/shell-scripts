2e59bdc788448f938274d881297a8da47e899b49
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-01-26 07:32:27 +0100

    feat: first draft of parsing cli

--- adopt-new-project.sh
+++ adopt-new-project.sh
@@ -25,22 +25,22 @@ DESCRIPTION
     - add this deploy key to your chezmoi dotfiles
 
 OPTIONS
-    -d, --deploy-key-dir  Specify the directory where to create the 
-                          GitHub deploy keypair. Default is
-                          "$DEFAULT_DEPLOY_KEYS_DIR"
-    -k, --keepass-db      Specify the path to the KeePass DB.
-                          Default is "$DEFAULT_KEEPASS_DB"
+    -d, --deploy-keys-dir  Specify the directory where to create the 
+                           GitHub deploy keypair. Default is
+                           "$DEFAULT_DEPLOY_KEYS_DIR"
+    -k, --keepass-db       Specify the path to the KeePass DB.
+                           Default is "$DEFAULT_KEEPASS_DB"
 
 FLAGS
-    -p, --private         GitHub repository shall be private
-    -K, --push-keepass    Commit and push the KeePass DB
-    -G, --push-chezmoi    Commit and push the chezmoi dotfiles
-    -h, --help            Prints this help text
+    -p, --private          GitHub repository shall be private
+    -K, --push-keepass     Commit and push the KeePass DB
+    -C, --push-chezmoi     Commit and push the chezmoi dotfiles
+    -h, --help             Prints this help text
 
 Example:
 $script_name ~/src/new-project \\
-  --deploy-keys-dir=~/.ssh/github_deploy_keys \\
-  --keepass-db=~/src/chazre/KeePassDB.kdbx \\
+  --deploy-keys-dir=~/path/to/deploy_keys \\
+  --keepass-db=~/path/to/KeePassDB.kdbx \\
   --private \\
   --push-keepass \\
   --push-chezmoi \\
@@ -49,6 +49,15 @@ EOF
 
 show_help() {
     echo "$help_text"
+}
+
+help_option_used() {
+    for arg in "$@"; do
+        if [ "$arg" = "-h" -o "$arg" = "--help" ]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 starts_with_a_hyphen() {
@@ -77,23 +86,73 @@ parse_optional_positional_argument() {
     fi
 }
 
+set_defaults() {
+    DEPLOY_KEYS_DIR=$DEFAULT_DEPLOY_KEYS_DIR
+    KEEPASS_DB=$DEFAULT_KEEPASS_DB
+    PRIVATE_GITHUB_REPOSITORY=0
+    PUSH_KEEPASS_DB=0
+    PUSH_CHEZMOI=0
+}
+
 parse_cli() {
+    if help_option_used "$@"; then
+        show_help
+        exit 0
+    fi
+
     PROJECT_DIR=$(parse_optional_positional_argument "$@")
     local user_specified_project_dir=$?
     if [ $user_specified_project_dir -eq 0 ]; then
         shift
     fi
-    while getopts ":d:k:ph" opt; do
+
+    while getopts ":d:k:pKCh-:" opt; do
         case $opt in
-            d) echo "Option -d with value $OPTARG" ;;
-            k) echo "Option -k with value $OPTARG" ;;
-            p) echo "Option -p" ;;
-            h) show_help; exit 0 ;;
+            d) DEPLOY_KEYS_DIR=$OPTARG; echo "Option -d with value $DEPLOY_KEYS_DIR" ;;
+            k) KEEPASS_DB=$OPTARG; echo "Option -k with value $KEEPASS_DB" ;;
+            p) echo "Option -p"; PRIVATE_GITHUB_REPOSITORY=1 ;;
+            K) echo "Option -K"; PUSH_KEEPASS_DB=1 ;;
+            C) echo "Option -C"; PUSH_CHEZMOI=1 ;;
+            h) show_help; exit 0 ;; # Redundant, but kept for completeness
+            -)
+                case "${OPTARG}" in
+                    deploy-key-dir=*)
+                        DEPLOY_KEYS_DIR=${OPTARG#*=}
+                        echo "Long option --deploy-key-dir with value $DEPLOY_KEYS_DIR"
+                        ;;
+                    keepass-db=*)
+                        KEEPASS_DB=${OPTARG#*=}
+                        echo "Long option --keepass-db with value $KEEPASS_DB"
+                        ;;
+                    private)
+                        PRIVATE_GITHUB_REPOSITORY=1
+                        echo "Long option --private"
+                        ;;
+                    push-keepass-db)
+                        PUSH_KEEPASS_DB=1
+                        echo "Long option --push-keepass-db"
+                        ;;
+                    push-chezmoi)
+                        PUSH_CHEZMOI=1
+                        echo "Long option --push-chezmoi"
+                        ;;
+                    *)
+                        echo "Unknown long option --${OPTARG}" >&2
+                        exit 1
+                        ;;
+                esac
+                ;;
             \?) echo "Invalid option: -$OPTARG" >&2; show_help; exit 1 ;;
             :) echo "Option -$OPTARG requires an argument" >&2; exit 1 ;;
         esac
     done
 }
 
+set_defaults
 parse_cli "$@"
-printf "project dir is \"%s\" after parsing the arguments.\n" $PROJECT_DIR
+printf "project dir is \"%s\"\n" $PROJECT_DIR
+printf "deploy keys dir is \"%s\"\n" $DEPLOY_KEYS_DIR
+printf "keepass db is \"%s\"\n" $KEEPASS_DB
+printf "private is \"%s\"\n" $PRIVATE_GITHUB_REPOSITORY
+printf "push keepass db \"%s\"\n" $PUSH_KEEPASS_DB
+printf "push chezmoi \"%s\"\n" $PUSH_CHEZMOI
