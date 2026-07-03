5fb350ddaa85d43bcf767a9c5b7b08084e747e48
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com[core]>
2026-07-03 09:21:39 +0200

    feat: kickstart-roll-snapshot feat: roll-snapshot now takes a positional argument

---
+++ kickstart-roll-snapshot.sh
@@ -0,0 +1,16 @@
+dataset=$1
+
+if [ -z "$dataset" ]; then
+    printf "%s\n" "Missing positional argument: dataset"
+    exit 1
+fi
+
+zfs snapshot -r "$dataset@7daysago"
+zfs snapshot -r "$dataset@6daysago"
+zfs snapshot -r "$dataset@5daysago"
+zfs snapshot -r "$dataset@4daysago"
+zfs snapshot -r "$dataset@3daysago"
+zfs snapshot -r "$dataset@2daysago"
+zfs snapshot -r "$dataset@yesterday"
+zfs snapshot -r "$dataset@today"
+
--- roll-snapshot.sh
+++ roll-snapshot.sh
@@ -1,10 +1,17 @@
-zfs destroy -r "zroot/home@7daysago"
-zfs rename -r "zroot/home@6daysago" "@7daysago"
-zfs rename -r "zroot/home@5daysago" "@6daysago"
-zfs rename -r "zroot/home@4daysago" "@5daysago"
-zfs rename -r "zroot/home@3daysago" "@4daysago"
-zfs rename -r "zroot/home@2daysago" "@3daysago"
-zfs rename -r "zroot/home@yesterday" "@2daysago"
-zfs rename -r "zroot/home@today" "@yesterday"
-zfs snapshot -r "zroot/home@today"
+dataset=$1
+
+if [ -z "$dataset" ]; then
+    printf "%s\n" "Missing positional argument: dataset"
+    exit 1
+fi
+
+zfs destroy -r "$dataset@7daysago"
+zfs rename -r "$dataset@6daysago" "@7daysago"
+zfs rename -r "$dataset@5daysago" "@6daysago"
+zfs rename -r "$dataset@4daysago" "@5daysago"
+zfs rename -r "$dataset@3daysago" "@4daysago"
+zfs rename -r "$dataset@2daysago" "@3daysago"
+zfs rename -r "$dataset@yesterday" "@2daysago"
+zfs rename -r "$dataset@today" "@yesterday"
+zfs snapshot -r "$dataset@today"
 
